228f87fa8989a7b90a3b4fef0adaa9fbb371f8e0
4hartman <4hartman@informatik.uni-hamburg.de>
2016-05-03 14:13:42 +0200

    Track - Material

---
+++ CycleCity_6/Materials/Track.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycleCity_6.Materials
+{
+    internal class Track
+    {
+        private int Id { get; set; }
+        private Array WayPoints { get; set; }
+    }
+}

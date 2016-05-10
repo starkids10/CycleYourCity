003841da3a9cab11fc3840059d4a048c3e50ae09
David <david@e-monteiro.de>
2016-05-10 14:41:36 +0200

    Restrukturierung der Anwendung.

---
+++ CycleCity_6/Services/TrackService.cs
@@ -0,0 +1,63 @@
+﻿using CycleCity_6.Materials;
+using Esri.ArcGISRuntime.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace CycleCity_6.Services
+{
+    internal class TrackService
+    {
+
+        private readonly List<Track> _tracks;
+
+        public TrackService()
+        {
+            _tracks = new List<Track>();
+        }
+
+        public event EventHandler<Track> TrackAddedEvent = delegate { };
+
+        /// <summary>
+        /// Returns all cyclists.
+        /// </summary>
+        /// <returns>all registered cyclists</returns>
+        public IEnumerable<Track> GetAllTracks()
+        {
+            Contract.Ensures(Contract.Result<IEnumerable<Track>>() != null);
+            Contract.Ensures(Contract.Result<IEnumerable<Track>>().Any());
+
+            return _tracks;
+        }
+
+        /// <summary>
+        /// Adds a new Tour to the list
+        /// </summary>
+        /// <param name="id">id of the track</param>
+        public void AddNewTrack(int id)
+        {
+            var newTrack = new Track(id,
+                new Polyline(new[] {new MapPoint(0, 0)}, SpatialReferences.Wgs84));
+
+            _tracks.Add(newTrack);
+
+            TrackAddedEvent(this, newTrack);
+        }
+
+        /// <summary>
+        /// Adds a new Tour to the list
+        /// </summary>
+        /// <param name="id">id of the track</param>
+        /// <param tour="tour">Polyline foor the track wich simulate the tour</param>
+        public void AddNewTrack(int id, Polyline tour)
+        {
+
+            var newTrack = new Track(id, tour);
+
+            _tracks.Add(newTrack);
+
+            TrackAddedEvent(this, newTrack);
+        }
+    }
+}
--- CycleCity_6/Tools/CyclistViewer/CyclistViewerView.xaml.cs
+++ CycleCity_6/Tools/CyclistViewer/CyclistViewerView.xaml.cs
@@ -12,33 +12,16 @@ namespace CycleCity_6.Tools.CyclistViewer
     {
         public CyclistViewerView()
         {
-            InitializeComponent();
+            InitializeComponent ();
 
-            GetViewModel().MapLayer = (GraphicsLayer)CycleMapView.Map.Layers["CyclistLayer"];
+            GetViewModel ().MapLayer = (GraphicsLayer)CycleMapView.Map.Layers["CyclistLayer"];
         }
 
         private CyclistViewerViewModel GetViewModel()
         {
-            Contract.Requires(DataContext is CyclistViewerViewModel);
+            Contract.Requires (DataContext is CyclistViewerViewModel);
 
             return (CyclistViewerViewModel)DataContext;
-        }
-
-        private void CyclistListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (GetViewModel().HasSelectedCyclist())
-            {
-                var cyclist = GetViewModel().SelectedCyclist;
-                CycleMapView.SetView(cyclist.Track.Tour);
-            }
-        }
-
-        private void CycleMapView_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            var z = e.GetPosition (this);
-            var mappoint = CycleMapView.ScreenToLocation (z);
-
-            Console.WriteLine ("" + mappoint.X + " " + mappoint.Y + " " + mappoint.Z);
         }
     }
 }
